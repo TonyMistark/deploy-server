f04b4636c2baeeb47b406daeed8347ed4e0cc120
ice <tony_mistark@163.com>
2018-12-17 18:41:33 +0800

    fix

--- app/sh/hexo_server_deploy.sh
+++ app/sh/hexo_server_deploy.sh
@@ -3,8 +3,8 @@
 echo "kill hexo"
 pkill hexo
 echo "cd /home/ice/projects/hexo-ablog/ablog/"
-cd /home/ice/projects/hexo-ablog/ablog/
+cd /home/ice/projects/hexo-ablog/ablog/ &
 echo "git pull git@github.com:TonyMistark/hexo-ablog.git"
-git pull git@github.com:TonyMistark/hexo-ablog.git
+git pull git@github.com:TonyMistark/hexo-ablog.git &
 echo "hexo server start"
 hexo server -i 0.0.0.0 -p 8989 &
