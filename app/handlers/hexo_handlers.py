76fdecbccfb3cc295dbfa27556f3a65176eb090e
ice <tony_mistark@163.com>
2018-12-17 15:37:35 +0800

    foix

--- app/handlers/hexo_handlers.py
+++ app/handlers/hexo_handlers.py
@@ -5,5 +5,5 @@ from app.handlers.base import HandlerBase
 class HexoHandler(HandlerBase):
 
     def get(self):
-        os.system("sh ../sh/hexo_server_deploy.sh")
+        os.system("../sh/hexo_server_deploy.sh")
         self.write("<h1> update hexo server success")
