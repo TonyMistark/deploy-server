89ddc5acb5b413c7620327648c16f7613ce9e5fe
ice <tony_mistark@163.com>
2018-12-18 18:05:23 +0800

    fix

--- app/handlers/hexo_handlers.py
+++ app/handlers/hexo_handlers.py
@@ -3,6 +3,10 @@ from app.handlers.base import HandlerBase
 
 
 class HexoHandler(HandlerBase):
+    def get(self):
+        os.system("sh ~/sh/hexo_server_deploy.sh")
+        self.write("<h1> update hexo server success")
+
     def post(self):
         os.system("sh ~/sh/hexo_server_deploy.sh")
         self.write("<h1> update hexo server success")
