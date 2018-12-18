ea6e8778c667e8ef08997666dc2d999f8170c590
ice <tony_mistark@163.com>
2018-12-18 10:29:44 +0800

    fix

--- app/sh/hexo_server_deploy.sh
+++ app/sh/hexo_server_deploy.sh
@@ -1,8 +1,6 @@
-echo "kill hexo"
-pkill hexo &
 echo "cd /home/ice/projects/hexo-ablog/ablog/"
 cd /home/ice/projects/hexo-ablog/ablog/
 echo "git pull git@github.com:TonyMistark/hexo-ablog.git"
 git pull &
 sleep 10s &
-hexo server -i 0.0.0.0 -p 8989 &
+sudo supervisorctl restart hexoserver &
