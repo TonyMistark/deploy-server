2e8f6ef018e30c6b556eb018b19ee860814a03c4
ice <tony_mistark@163.com>
2019-09-19 11:52:26 +0800

    add  config file

--- app/handlers/hexo_handlers.py
+++ app/handlers/hexo_handlers.py
@@ -1,12 +1,13 @@
 import os
 from app.handlers.base import HandlerBase
+from config import HexoConfig
 
 
 class HexoHandler(HandlerBase):
     def get(self):
-        os.system("sh ~/sh/hexo_server_deploy.sh")
-        self.write("<h1> update hexo server success")
+        os.system(f"sh {HexoConfig.hexo_server_deploy_abs_path}")
+        self.write("<h1> update hexo server success.")
 
     def post(self):
-        os.system("sh ~/sh/hexo_server_deploy.sh")
-        self.write("<h1> update hexo server success")
+        os.system(f"sh {HexoConfig.hexo_server_deploy_abs_path}")
+        self.write("<h1> update hexo server success!")
---
+++ config_sample.py
@@ -0,0 +1,3 @@
+class HexoConfig(object):
+    # 自动部署hexo服务脚本绝对路径
+    hexo_server_deploy_abs_path = ""
